ec0f2d75801e8e55de58ce1b85007222ff9e2598
barny-dev <barnaba.piotrowski@gmail.com>
2025-07-04 16:30:54 +0000

    stuff

--- lib/Web/Convent/Storage/FilePage.hs
+++ lib/Web/Convent/Storage/FilePage.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, AllowAmbiguousTypes, GeneralizedNewtypeDeriving #-}
 module Web.Convent.Storage.FilePage
   ( read
   , write
@@ -11,8 +11,9 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import qualified System.IO as IO
 import Control.Exception (handle, evaluate)
+import Control.Monad.Trans.Except (ExceptT (..), except, withExceptT, throwE, catchE, runExceptT)
 
-newtype Index = Offset Int deriving (Show, Eq)
+newtype Index = Index Int deriving (Show, Eq)
 newtype Size = Size Int deriving (Show, Eq)
 type Ptr = (Index, Size)
 -- | Errors that can occur when reading pages from a file
@@ -30,23 +31,27 @@ data ReadError =
 -- @returns Either an error describing why reading failed, or the raw page data
 --
 -- Returns an error if the file is too small or if the page index is invalid.
-read :: IO.Handle -> FilePagePtr -> IO (Either ReadError ByteString)
-read fh ((Index ix), (Size sz)) = (handle (\err -> ReadIOError err) read') >>= evaluate
-  where read' = do
-    if pageIdx < 0 
-      then return $ Left $ InvalidPageOffset pageIdx
-      else do
-        fileSize <- IO.hFileSize hf
-        let offset = fromIntegral pageIdx * fromIntegral pageSize
-        let requiredSize = offset + fromIntegral pageSize
-        if fileSize < requiredSize
-          then return $ Left $ FileTooSmall (fromIntegral requiredSize) (fromIntegral fileSize)
-          else do
-            IO.hSeek fh IO.AbsoluteSeek offset
-            pageData <- ByteString.hGet fh pageSize
-            return Right $! pageData
+read :: IO.Handle -> Ptr -> IO (Either ReadError ByteString)
+read fh (Index ix, Size ps) = (handle (\err -> ReadIOError err) read') >>= evaluate
+  where read' = runExceptT $! do 
+                if po < 0 
+                  then throwE ReadInvalidPageIndex ix
+                  else return ()
+                fileSize <- lift IO.hFileSize hf
+                let offset = fromIntegral ix * fromIntegral ps
+                    requiredSize = offset + fromIntegral ps
+                if fileSize < requiredSize
+                  then throwE $ ReadFileTooSmall (fromIntegral requiredSize) (fromIntegral fileSize)
+                  else return ()
+                lift $ IO.hSeek fh IO.AbsoluteSeek offset
+                pageData <- lift $ ByteString.hGet fh (fromIntegral ps)
+                return Right $! pageData
 
-data WriteError = WriteIOError Prelude.IOError deriving (Show, Eq)
+data WriteError = 
+   WriteInvalidPageIndex Index | 
+   WriteFileTooSmall Integer Integer |
+   WritePageSizeMismatch Size Size |
+   WriteIOError Prelude.IOError deriving (Show, Eq)
 
 -- | Writes a page to a file at the specified index.
 --
@@ -58,14 +63,30 @@ data WriteError = WriteIOError Prelude.IOError deriving (Show, Eq)
 --
 -- Will overwrite any existing data at that position. The pageData should
 -- be exactly pageSize bytes, but this is not enforced by this function.
-write :: IO.Handle -> FilePagePtr -> ByteString -> IO (Either WriteError ())
-write fh pageIdx pageSize pageData = (handle (\err -> IOError err) write') >>= evaluate
-  where write' = do
-    let offset = fromIntegral pageIdx * fromIntegral pageSize
-    IO.hSeek fh IO.AbsoluteSeek offset
-    ByteString.hPut fh pageData
-
+write :: IO.Handle -> Ptr -> ByteString -> IO (Either WriteError ())
+write fh (Index ix, Size ps) pageData = (handle (\err -> WriteIOError err) write') >>= evaluate
+  where write' = runExceptT $! do
+                 if ix < 0 
+                   then throwE WriteInvalidPageOffset ix
+                   else return ()
+                 if fromIntegral ps /= ByteString.length pageData 
+                   then throw WritePageSizeMismatch ps (fromIntegral $ ByteString.length pageData) 
+                   else return ()
+                 let offset = fromIntegral ix * fromIntegral ps
+                 lift $ IO.hSeek fh IO.AbsoluteSeek offset
+                 lift $ ByteString.hPut fh pageData
+          
 class FilePage a where
   type FilePageLoadError a :: *
-  type FilePageSaveError
-  toByteString :: a -> Either 
+  type FilePageSaveError a :: *
+  toByteString :: a -> Either (FilePageSaveError a) ByteString
+  fromByteString :: ByteString -> Either (FilePageLoadError a) a
+  mapWriteError :: WriteError -> FilePageSaveError a
+  mapReadError :: ReadError -> FilePageLoadError a
+
+loadPage :: FilePage a => IO.Handle -> Ptr -> IO (Either (FilePageLoadError a) a)
+loadPage fh ptr = do
+    result <- read fh ptr
+    return $! case result of
+        Left err -> Left $! mapReadError err
+        Right byteData -> fromByteString $! byteData
--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
   deps = [
+    pkgs.haskellPackages.text_2_1_1
+    pkgs.haskellPackages.bytestring_0_12_1_0
     pkgs.haskellPackages.hspec
     pkgs.haskellPackages.temporary
     pkgs.haskellPackages.ghc
